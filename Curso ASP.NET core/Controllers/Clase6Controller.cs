76edf6bd970eb97eff79bb7872b5d7c65140a28a
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2021-07-26 19:49:04 -0400

    Se pasó la clase 6.

---
+++ Curso ASP.NET core/Controllers/Clase6Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Curso_ASP.NET_core.Models;
+
+namespace Curso_ASP.NET_core.Controllers
+{
+    public class Clase6Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index", new Cuenta());
+        }
+
+        public IActionResult Respuesta(Cuenta cuentaQueLlega) {
+            if (ModelState.IsValid)
+            {
+                ViewBag.cuen = cuentaQueLlega;
+                return View("Exitoclase6");
+            }
+            else
+                return View("Index");
+        }
+    }
+}
---
+++ Curso ASP.NET core/Models/Cuenta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Curso_ASP.NET_core.Models
+{
+    public class Cuenta
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        public string nombre { get; set; }
+        [Required]
+        [MinLength(3)]
+        [MaxLength(10)]
+        public string password { get; set; }
+        [Required]
+        [Range(18, 55)]
+        public int edad { get; set; }
+        [Required]
+        [EmailAddress]
+        public string correo { get; set; }//abc@bla.algo
+        [Url]
+        public string pagina { get; set; }//cosa://algo.noseque.bla
+    }
+}
