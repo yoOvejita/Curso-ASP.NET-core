35140347ab1e137fc76a2e5efb4071214688e1e0
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2021-07-26 10:52:04 -0400

    Agregar archivos de proyecto.

---
+++ Curso ASP.NET core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Curso_ASP.NET_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
