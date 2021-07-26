3ecf112bdf9a13befba55140ab288f1d847f3873
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2021-07-26 12:00:37 -0400

    Agregando resumen de lo avanzado hasta la clase 2

---
+++ Curso ASP.NET core/Controllers/Clase2Controller.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_ASP.NET_core.Controllers
+{
+    public class Clase2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            //Estas 6 líneas se explican en la página /Clase2/Index
+            ViewBag.texto = "Un buen texto que tengo acá";
+            ViewBag.numero = 777;
+            ViewBag.caracter = 'F';
+            ViewBag.arreglo = new string[]{ "a", "b", "c", "d"};
+            ViewBag.boole = true;
+            ViewBag.decim = 3.1416;
+            return View();
+        }
+    }
+}
