1e322f4696addeece9d638b8df19fee45def5495
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2021-07-29 20:06:57 -0400

    Clae 9 terminada.

---
+++ Curso ASP.NET core/Controllers/Clase9Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso_ASP.NET_core.Controllers
+{
+    public class Clase9Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string user, string pass)
+        {
+            string userValido = "PEPEX";
+            string passValido = "abc123";
+            if (user.Equals(userValido) && pass.Equals(passValido))
+            {
+                HttpContext.Session.SetString("usuario", user);
+                return View("Loggeado");
+            }
+            else {
+                ViewBag.mensaje = "El nombre de usuario y/o la contraseña no son validos.";
+                return View("Index");
+            }
+        }
+
+        public IActionResult Logout() {
+            HttpContext.Session.Remove("usuario");
+            return View("Index");
+        }
+
+        public IActionResult Ejemplo1() {
+            return View();
+        }
+        public IActionResult Ejemplo2()
+        {
+            return View();
+        }
+        public IActionResult Ejemplo3()
+        {
+            return View();
+        }
+    }
+}
