c610a7811055bb9d7575ac473de9c90675a23982
Ruben Gonzalo Soria Soria <rusovsky_c@hotmail.com>
2021-08-04 21:28:39 -0400

    Clase 13 total

---
+++ Curso ASP.NET core/TagHelpers/Ejemplo1.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Curso_ASP.NET_core.TagHelpers
+{
+    [HtmlTargetElement("ejemplo1",TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class Ejemplo1 : TagHelper
+    {
+        public override void Process(TagHelperContext contexto, TagHelperOutput salida) {
+            salida.TagName = "";
+            salida.Content.SetHtmlContent("<h3>Hola Pepe</h3>");
+        }
+    }
+}
---
+++ Curso ASP.NET core/TagHelpers/Ejemplo2.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Curso_ASP.NET_core.TagHelpers
+{
+    [HtmlTargetElement("ejemplo2", TagStructure = TagStructure.NormalOrSelfClosing)]
+    public class Ejemplo2 : TagHelper
+    {
+        [HtmlAttributeName("a")]
+        public int a { get; set; }
+        [HtmlAttributeName("b")]
+        public int b { get; set; }
+        public override void Process(TagHelperContext contexto, TagHelperOutput salida)
+        {
+            salida.TagName = "";
+            salida.Content.SetHtmlContent("<h3>" +(a+b).ToString()+ "</h3>");
+        }
+    }
+}
